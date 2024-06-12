c9b8bda5eac0f2150ce8f216ebd0b17b8b0ce65f
DaggerLee <93747470+DaggerLee@users.noreply.github.com>
2024-06-11 18:13:07 -0700

    Update server.py

--- server.py
+++ server.py
@@ -8,32 +8,44 @@ def hello():
     distance1 = int(request.args.get("distance1"))
     distance2 = int(request.args.get("distance2"))
 
-    car_both = """
-        Car:
+    car = """
+    Car:
 
-        ____________________
-    |                    || !!!
-    |                    || !!!
-    |                    || !!!
-    |____________________|| !!!
-                        !!!!
-                        !!!!
-        """
-    car_left = """
-        Car:
-
-        ____________________
+     ____________________
     |                    ||
     |                    ||
     |                    ||
     |____________________||
-                        !!!!
-                        !!!!
+                    
+                    
+        """
+    
+    car_both = """
+    Car:
+
+     ____________________
+    |                    || !!!
+    |                    || !!!
+    |                    || !!!
+    |____________________|| !!!
+                    !!!!
+                    !!!!
+        """
+    car_left = """
+    Car:
+
+     ____________________
+    |                    ||
+    |                    ||
+    |                    ||
+    |____________________||
+                    !!!!
+                    !!!!
         """
     car_back = """
-        Car:
+    Car:
 
-        ____________________
+     ____________________
     |                    || !!!
     |                    || !!!
     |                    || !!!
@@ -42,18 +54,20 @@ def hello():
 
     stop = """
     SSSSS  TTTTTTT  OOOOO  PPPPPP
-    S         T     O     O P     P
-    SSSSS    T     O     O PPPPPP
-        S   T     O     O P
-    SSSSSS    T      OOOOO  P
+    S         T    O     O P     P
+    SSSSS     T    O     O PPPPPP
+        S     T    O     O P
+    SSSSS     T     OOOOO  P
     """
 
     if distance1 <= 10 or distance2 <= 10:
         print(stop)
-    else if distance1 <= 30 and distance2 <= 30:
+    elif distance1 <= 30 and distance2 <= 30:
         print(car_both)
-    else if distance1 <= 30:
+    elif distance1 <= 30:
         print(car_back)
-    else if distance2 <= 30:
+    elif distance2 <= 30:
         print(car_left)
+    else:
+        print(car)
 
